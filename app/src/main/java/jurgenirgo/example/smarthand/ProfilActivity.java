606a047a1453d1037cc60f3978751a6190bca23a
Jurgen <jurgenirgof@gmail.com>
2019-08-22 02:03:54 +0700

    update

--- app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
+++ app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 public class ProfilActivity extends AppCompatActivity {
 
