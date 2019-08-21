6788af588402da1c6a9e364e8af0fcb2f699312f
Jurgen <jurgenirgof@gmail.com>
2019-08-22 01:10:10 +0700

    update

--- app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
+++ app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
@@ -1,5 +1,6 @@
 package jurgenirgo.example.smarthand;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
