6dad7abcdb3c02b3e287040efb3171e1b0e7254b
Jurgen <jurgenirgof@gmail.com>
2019-08-22 01:51:15 +0700

    update

--- app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
+++ app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
@@ -12,13 +12,16 @@ public class ProfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
-        if (getSupportActionBar()!= null)
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home)
             finish();
+
         return super.onOptionsItemSelected(item);
     }
 }
