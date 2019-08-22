26aa404eadfced0223cdc8ba5e8a3852184ba11c
Jurgen <jurgenirgof@gmail.com>
2019-08-22 14:07:51 +0700

    update

--- app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
+++ app/src/main/java/jurgenirgo/example/smarthand/ProfilActivity.java
@@ -1,27 +1,34 @@
 package jurgenirgo.example.smarthand;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class ProfilActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        toolbar = findViewById(R.id.toolbar);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
     }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home)
-            finish();
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
         return super.onOptionsItemSelected(item);
     }
 }
