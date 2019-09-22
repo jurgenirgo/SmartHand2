19d6d207a38dbbcb091aebb3909016d84ae7d60e
Jurgen <jurgenirgof@gmail.com>
2019-09-23 00:43:53 +0700

    update

--- app/src/main/java/jurgenirgo/example/smarthand/MenuVideoActivity.java
+++ app/src/main/java/jurgenirgo/example/smarthand/MenuVideoActivity.java
@@ -1,7 +1,10 @@
 package jurgenirgo.example.smarthand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MenuVideoActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MenuVideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_video);
+
+        ImageView video1 = (ImageView) findViewById(R.id.btn_video_1);
+        ImageView video2 = (ImageView) findViewById(R.id.btn_Video_2);
+
+        video1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int1 =new Intent(MenuVideoActivity.this, Video1Activity.class);
+                startActivity(int1);
+            }
+        });
+
     }
 }
